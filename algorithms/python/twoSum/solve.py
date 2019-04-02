620812559faa2a4b0c49370322824480380ef7f1
NakanoMakoto <m.nakano8649@gmail.com>
2019-04-02 23:49:14 +0900

    refuctor time problem

--- algorithms/python/twoSum/solve.py
+++ algorithms/python/twoSum/solve.py
@@ -1,8 +1,9 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums)):
-            for j in range(0, len(nums)):
-                if i == j:
-                    continue
-                if nums[j] == target - nums[i]:
-                    return [i, j]
+    def twoSum(self, nums, target) :
+        obj = {}
+        for i,v in enumerate(nums):
+            rest = target - v
+            if rest in obj:
+                return [obj[rest],i]
+            else:
+                obj[v] = i
